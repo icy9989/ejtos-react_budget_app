1a6f6cffbbdbe9169608c86ba3359e9a38d7187f
Khaing Min Htwe <90689025+icy9989@users.noreply.github.com>
2024-02-13 10:17:25 +0630

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 // Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
@@ -17,18 +21,31 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
+                            
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <div>
+                                <ExpenseList />
+                            </div>
                         }         
 
                         {
@@ -37,6 +54,9 @@ const App = () => {
 
                         {
                             /* Add AllocationForm component here under */
+                            <div>
+                                <AllocationForm />
+                            </div>
                         }        
 
                 </div>
